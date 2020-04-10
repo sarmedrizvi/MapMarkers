f219907756c2bb0c5dbf549a6c8bc9c6c663918d
sarmedrizvi <Max@Max-43.bizzwc.local>
2020-04-10 15:00:42 -0700

    Till insta card

--- components/InstaCard.vue
+++ components/InstaCard.vue
@@ -6,7 +6,6 @@
       </v-list-item-avatar>
       <v-list-item-content>
         <v-list-item-title class="headline">Our Changing Planet</v-list-item-title>
-        <v-list-item-subtitle>by Kurt Wagner</v-list-item-subtitle>
       </v-list-item-content>
     </v-list-item>
 
@@ -14,14 +13,17 @@
     <v-btn icon>
       <v-icon>mdi-heart</v-icon>
     </v-btn>
+
     <v-btn icon>
-      <v-icon>mdi-share-variant</v-icon>
+      <v-icon>mdi-comment</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>mdi-telegram</v-icon>
     </v-btn>
     <v-card-text>Visit ten places on our planet that are undergoing the biggest changes today.</v-card-text>
 
     <v-card-actions>
-      <v-btn text color="deep-purple accent-4">Read</v-btn>
-      <v-btn text color="deep-purple accent-4">Bookmark</v-btn>
+      <v-text-field placeholder="Comment" filled rounded dense style="width:100%"></v-text-field>
       <v-spacer></v-spacer>
     </v-card-actions>
   </v-card>
